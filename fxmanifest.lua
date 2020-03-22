51d190ee6bcfea3077f96d381da979a1315bc0e7
Xstasy <x@gbps.io>
2020-03-22 21:01:29 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'adamant'
+game 'gta5'
+
+name 'fivem-ts-typeorm-boilerplate'
+version '1.0.0'
+description 'FiveM TypeScript TypeORM Boilerplate'
+author 'Xstasy'
+url 'https://github.com/Xstasy/fivem-ts-typeorm-boilerplate'
+
+-- REQUIRED FILES
+files {
+
+}
+
+-- LOADSCREEN
+-- loadscreen 'ui/loading/load.html'
+-- loadscreen_manual_shutdown 'yes'
+
+-- NUI UI PAGE
+--ui_page 'ui/main/main.html'
+
+
+client_script 'client.js'
+server_script 'server.js'
